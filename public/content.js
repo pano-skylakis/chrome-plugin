20f54dd4add6fdb95f060a502fb5f43b226bad7d
pano-skylakis <pano_skylakis@outlook.com>
2019-09-09 14:34:36 +1200

    test

--- public/content.js
+++ public/content.js
@@ -1,11 +1,15 @@
 chrome.runtime.onMessage.addListener(request => {
   if (request.type === 'showPremium') {
-    const pTags = document.getElementsByTagName('p').removeAttribute('class', 'style')
+    const pTags = document.getElementsByTagName('p')
     
-    return pTags.map(tag => {
-      return tag.removeAttribute('class')
-    }).map(classRemovedTag => {
-      return classRemovedTag.removeAttribute('style')
-    })
+
+    for (let i = 0; i < pTags.length; i ++) {
+      return pTags[i].removeAttribute('class')
+    }
+  //   return pTags.map(tag => {
+  //     return tag.removeAttribute('class')
+  //   }).map(classRemovedTag => {
+  //     return classRemovedTag.removeAttribute('style')
+  //   })
   }
 })
